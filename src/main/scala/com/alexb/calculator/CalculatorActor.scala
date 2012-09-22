8bf37bc41993b91a8624436448087b221d290ac5
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-09-22 15:26:02 +0400

    Remove unnecessary "protected" keyword.

--- src/main/scala/com/alexb/calculator/CalculatorActor.scala
+++ src/main/scala/com/alexb/calculator/CalculatorActor.scala
@@ -4,7 +4,7 @@ import akka.actor.Actor
 
 class CalculatorActor extends Actor {
 
-	protected def receive = {
+	def receive = {
 		case AddCommand(a, b) => sender ! a + b
 		case SubtractCommand(a, b) => sender ! a - b
 		case DivideCommand(a, b) => sender ! (try { Left(a / b) } catch { case e: ArithmeticException => Right(e) })
