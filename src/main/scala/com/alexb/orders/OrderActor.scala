bed39f266f867e7dc95ff44ea7017a41e45a4aa6
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-10-14 16:52:54 +0400

    val -> def

--- src/main/scala/com/alexb/orders/OrderActor.scala
+++ src/main/scala/com/alexb/orders/OrderActor.scala
@@ -11,38 +11,38 @@ class OrderActor(collection: MongoCollection) extends Actor with FutureUtils {
 	def receive = {
 		case cmd: AddOrderCommand => wrapInFuture { saveOrder(Order(UUID.randomUUID.toString, cmd.clientId, List(), cmd.notes)) }
 		case cmd: DeleteOrderCommand => wrapInFuture { deleteOrder(cmd.orderId) }
-		case cmd: OrderByIdQuery => answerWithFutureResult { findOrder(cmd.orderId)	} 
+		case cmd: OrderByIdQuery => answerWithFutureResult { findOrder(cmd.orderId) }
 		case cmd: OrdersByClientIdQuery => answerWithFutureResult { findOrdersByClient(cmd.clientId) }
 	}
-	
+
 	private def saveOrder(order: Order) = {
 		collection += MongoDBObject("_id" -> order.orderId,
-									"clientId" -> order.clientId,
-									"items" -> order.items,
-									"notes" -> order.notes)
+			"clientId" -> order.clientId,
+			"items" -> order.items,
+			"notes" -> order.notes)
 	}
-	
+
 	private def deleteOrder(orderId: String) = {
 		collection -= MongoDBObject("_id" -> orderId)
 	}
-	
-	private def findOrder(orderId: String) = 
+
+	private def findOrder(orderId: String) =
 		collection
 			.findOne(MongoDBObject("_id" -> orderId))
 			.map(toOrder)
-	
-	private def findOrdersByClient(clientId: String) = 
+
+	private def findOrdersByClient(clientId: String) =
 		collection
 			.find(MongoDBObject("clientId" -> clientId))
 			.map(toOrder)
 			.toList
-	
-	private val toOrder: DBObject => Order =
-		d => Order(d.getAs[String]("_id").get,
-					d.getAs[String]("clientId").get,
-					d.getAs[Iterable[DBObject]]("items").get.toList.map(toOrderItem),
-					d.getAs[String]("notes").getOrElse(""))
-	
-	private val toOrderItem: DBObject => OrderItem =
-		d => OrderItem(d.getAs[String]("itemId").get, d.getAs[Int]("quantity").get)
+
+	private def toOrder(d: DBObject): Order =
+		Order(d.getAs[String]("_id").get,
+			d.getAs[String]("clientId").get,
+			d.getAs[Iterable[DBObject]]("items").get.toList.map(toOrderItem),
+			d.getAs[String]("notes").getOrElse(""))
+
+	private def toOrderItem(d: DBObject): OrderItem =
+		OrderItem(d.getAs[String]("itemId").get, d.getAs[Int]("quantity").get)
 }
