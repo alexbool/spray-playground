8cc72d04d12ba87a604a25405ef487bcf9bc5211
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-10-24 20:19:58 +0400

    Use ElasticSearch utilities in OrderSearchActor

--- src/main/scala/com/alexb/orders/OrderSearchActor.scala
+++ src/main/scala/com/alexb/orders/OrderSearchActor.scala
@@ -3,28 +3,26 @@ package com.alexb.orders
 import akka.actor.Actor
 import akka.actor.ActorLogging
 import spray.json._
+import com.alexb.elasticsearch.ElasticSearch
 import com.alexb.utils.{ FutureUtils, PageInfo }
 import org.elasticsearch.client.Client
 import org.elasticsearch.index.query.QueryBuilders._
 
-class OrderSearchActor(client: Client, index: String) extends Actor with OrderMarshallers with FutureUtils with ActorLogging {
+class OrderSearchActor(client: Client, index: String) extends Actor with ElasticSearch with OrderMarshallers with FutureUtils with ActorLogging {
 
+	implicit val esClient = client
+	
 	def receive = {
 		case cmd: SearchOrdersByNotesQuery => answerWithFutureResult { searchByNotes(cmd.query, cmd.page) }
 	}
 	
-	def searchByNotes(query: String, page: PageInfo) = {
-		client.prepareSearch(index)
-			.setQuery(fuzzyQuery("notes", query))
-			.setFrom(page.skip)
-			.setSize(page.size)
-			.execute()
-			.actionGet()
-			.getHits()
-			.getHits()
-			.map(h => h.getSourceAsString()
+	def searchByNotes(query: String, page: PageInfo) = 
+		index(index)
+			.find(fuzzyQuery("notes", query))
+			.drop(page.skip)
+			.take(page.size)
+			.map(_.getSourceAsString()
 					.replace("\"_id\"", "\"orderId\"") // Bad hack
 					.asJson.convertTo[Order])
 			.toSeq
-	}
 }
