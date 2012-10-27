09b141b41cce2fc5bf6d94c9455d586b59d8d076
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-10-28 00:18:39 +0400

    Add JodaFormats

---
+++ src/test/scala/com/alexb/utils/JodaFormatsSpec.scala
@@ -0,0 +1,19 @@
+package com.alexb.utils
+
+import org.scalatest._
+import org.joda.time.Instant
+import spray.json.{ JsString, JsNumber }
+
+class JodaFormatsSpec extends WordSpec with MustMatchers with JodaFormats {
+
+	"Joda JsonFormatter" must {
+		"serialize Instants" in {
+			val instant = new Instant(1351368713660L)
+			instantFormat.write(instant) must equal(JsString("2012-10-27T20:11:53Z"))
+		}
+		"deserialize Instants" in {
+			instantFormat.read(JsString("2012-10-27T20:11:53Z")) must equal(new Instant(1351368713000L))
+			evaluating { instantFormat.read(JsNumber(10)) } must produce [Exception]
+		}
+	}
+}
