df750d2e584d4cc0d9c80821197736b14d75e01b
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-10-30 20:40:53 +0400

    Add ErrorDescription case class and marshallers

---
+++ src/main/scala/com/alexb/utils/ErrorDescription.scala
@@ -0,0 +1,11 @@
+package com.alexb.utils
+
+import spray.json.DefaultJsonProtocol
+
+case class ErrorDescription(description: String)
+
+trait ErrorDescriptionMarshallers extends DefaultJsonProtocol {
+	implicit val errorDescriptionFormat = jsonFormat1(ErrorDescription)
+}
+
+object ErrorDescriptionMarshallers extends ErrorDescriptionMarshallers
