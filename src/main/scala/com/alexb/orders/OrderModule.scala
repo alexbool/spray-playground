93177c89d2d72b928e56da3d348c528a1b3f2748
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-11-11 20:54:38 +0400

    Sort out dependencies in OrderModule (#5)

--- src/main/scala/com/alexb/orders/OrderModule.scala
+++ src/main/scala/com/alexb/orders/OrderModule.scala
@@ -3,14 +3,11 @@ package com.alexb.orders
 import akka.actor.{ ActorSystem, Props }
 import com.mongodb.casbah.MongoCollection
 import org.elasticsearch.client.Client
-import com.alexb.oauth.{ OAuthTokenValidator, User }
+import com.alexb.main.context._
 
 trait OrderModule extends OrderService {
-
-	def actorSystem: ActorSystem
+	this: MongoContext with ElasticSearchContext with OAuthContext =>
 	def collection: MongoCollection
-	def elasticSearchClient: Client
-	def tokenValidator: OAuthTokenValidator[User]
 
 	private lazy val orderActorRef = actorSystem.actorOf(
 		props = Props(new OrderActor(collection)))
