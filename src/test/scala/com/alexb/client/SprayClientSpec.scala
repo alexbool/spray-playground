0abcf079654a12ba5bf85ecb649425038c3a071a
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-11-14 19:51:36 +0400

    Add SprayClientSpec

---
+++ src/test/scala/com/alexb/client/SprayClientSpec.scala
@@ -0,0 +1,29 @@
+package com.alexb.client
+
+import org.scalatest._
+import akka.actor.{ ActorSystem, Props }
+import akka.dispatch.Await
+import akka.util.duration._
+import spray.io.IOBridge
+import spray.can.client.HttpClient
+import spray.client.HttpConduit
+import spray.client.HttpConduit._
+
+class SprayClientSpec extends WordSpec with MustMatchers {
+  val system = ActorSystem("test")
+  val ioBridge = new IOBridge(system).start()
+  val httpClient = system.actorOf(
+    props = Props(new HttpClient(ioBridge)),
+    name = "test-http-client")
+  val conduit = system.actorOf(
+    props = Props(new HttpConduit(httpClient, "173.194.32.134" /* this is google.com's IP */, 80)),
+    name = "test-http-conduit"
+  )
+  val pipeline = sendReceive(conduit)
+
+  "Spray client" must {
+    "download content in reasonable time" in {
+      Await.result(pipeline(Get("/")), 3 seconds)
+    }
+  }
+}
