091f3e79d25303e9ea0621cb31ba416b6e9cdac1
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-11-14 23:05:29 +0400

    Trivial: tabs => spaces

--- src/test/scala/com/alexb/user/MongoUserRepositorySpec.scala
+++ src/test/scala/com/alexb/user/MongoUserRepositorySpec.scala
@@ -8,27 +8,27 @@ import org.joda.time.Instant
 
 class MongoUserRepositorySpec extends WordSpec with MustMatchers with Config with MongoSupport with MongoUserRepository with BeforeAndAfterEach {
 
-	override def beforeEach {
-		clear()
-		save(user)
-	}
+  override def beforeEach {
+    clear()
+    save(user)
+  }
 
-	val user = User("alexb", "changeme", Instant.now)
+  val user = User("alexb", "changeme", Instant.now)
 
-	"MongoUserRepository" ignore {
-		"save and find users" in {
-			find("alexb") must equal (Some(user))
-		}
-		"check user credentials" in {
-			checkCredentials(user.username, user.password) must be (true)
-			checkCredentials(user.username, "wrong password") must be (false)
-		}
-		"check for free usernames" in {
-			checkUsernameFree(user.username) must be (false)
-			checkUsernameFree("a_free_username") must be (true)
-		}
-		"not insert duplicate users" in {
-			evaluating { save(user) } must produce [DuplicateUsernameException]
-		}
-	}
+  "MongoUserRepository" ignore {
+    "save and find users" in {
+      find("alexb") must equal (Some(user))
+    }
+    "check user credentials" in {
+      checkCredentials(user.username, user.password) must be (true)
+      checkCredentials(user.username, "wrong password") must be (false)
+    }
+    "check for free usernames" in {
+      checkUsernameFree(user.username) must be (false)
+      checkUsernameFree("a_free_username") must be (true)
+    }
+    "not insert duplicate users" in {
+      evaluating { save(user) } must produce [DuplicateUsernameException]
+    }
+  }
 }
