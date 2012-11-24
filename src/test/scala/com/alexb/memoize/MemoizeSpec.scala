cc6a9333ab07b055c8d1f5dd0266bd97d3559683
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-11-24 18:09:23 +0400

    Test memoize with ScalaMock (#9)

--- src/test/scala/com/alexb/memoize/MemoizeSpec.scala
+++ src/test/scala/com/alexb/memoize/MemoizeSpec.scala
@@ -1,40 +1,53 @@
 package com.alexb.memoize
 
 import org.scalatest._
+import org.scalamock.scalatest.MockFactory
 import Memoize._
 
-class MemoizeSpec extends WordSpec with MustMatchers {
+class MemoizeSpec extends WordSpec with MustMatchers with MockFactory {
 
 	implicit val cacheManager = new ConcurrentHashMapCacheManager
 	
 	"Memoize function" must {
 		"cache no-arg functions" in {
-			def memoized = memoize { () => 1 }
+      val m = mockFunction[Int]
+      m.expects().returns(1).once
+			val memoized = memoize(m)
 			memoized() must equal(1)
 			memoized() must equal(1)
 		}
 		"cache one-arg functions" in {
-			def memoized = memoize { a: Int => a + 1 }
+      val m = mockFunction[Int, Int]
+      m.expects(*).returns(2).once
+      val memoized = memoize(m)
 			memoized(1) must equal(2)
 			memoized(1) must equal(2)
 		}
 		"cache two-arg functions" in {
-			def memoized = memoize { (a: Int, b: Int) => a + b }
+      val m = mockFunction[Int, Int, Int]
+      m.expects(*, *).returns(3).once
+      val memoized = memoize(m)
 			memoized(1, 2) must equal(3)
 			memoized(1, 2) must equal(3)
 		}
 		"cache three-arg functions" in {
-			def memoized = memoize { (a: Int, b: Int, c: Int) => a + b + c }
+      val m = mockFunction[Int, Int, Int, Int]
+      m.expects(*, *, *).returns(3).once
+      val memoized = memoize(m)
 			memoized(1, 1, 1) must equal(3)
 			memoized(1, 1, 1) must equal(3)
 		}
 		"cache four-arg functions" in {
-			def memoized = memoize { (a: Int, b: Int, c: Int, d: Int) => a + b + c + d }
+      val m = mockFunction[Int, Int, Int, Int, Int]
+      m.expects(*, *, *, *).returns(4).once
+      val memoized = memoize(m)
 			memoized(1, 1, 1, 1) must equal(4)
 			memoized(1, 1, 1, 1) must equal(4)
 		}
 		"cache no-arg functions with cache name and key" in {
-			def memoized = memoize("Cache name", "key", () => 1)
+      val m = mockFunction[Int]
+      m.expects().returns(1).once
+			def memoized = memoize("Cache name", "key", m)
 			memoized() must equal(1)
 			memoized() must equal(1)
 		}
