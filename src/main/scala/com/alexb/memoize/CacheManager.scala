7379c1470de60cafae361664bb2d34096b9d3bfc
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-12-05 20:33:26 +0400

    Add getAsync method (#17)

--- src/main/scala/com/alexb/memoize/CacheManager.scala
+++ src/main/scala/com/alexb/memoize/CacheManager.scala
@@ -1,6 +1,13 @@
 package com.alexb.memoize
 
+import concurrent.{ExecutionContext, Future}
+
 trait CacheManager {
   def get[T](cacheName: String, key: Any): Option[T]
+
+  // A very basic implementation, override if your cache has async nature
+  def getAsync[T](cacheName: String, key: Any)(implicit ctx: ExecutionContext): Future[Option[T]] =
+    Future { get[T](cacheName, key) }
+
   def put(cacheName: String, key: Any, value: Any)
 }
--- src/test/scala/com/alexb/memoize/CacheManagerSpec.scala
+++ src/test/scala/com/alexb/memoize/CacheManagerSpec.scala
@@ -1,15 +1,21 @@
 package com.alexb.memoize
 
 import org.scalatest._
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits.global
 
 trait CacheManagerSpec extends WordSpec with MustMatchers {
 	def name: String
 	def cacheManager: CacheManager
-	
+
 	name must {
 		"implement get method" in {
 			cacheManager.get("", "a key") must equal (None)
 		}
+    "implement getAsync method" in {
+      Await.result(cacheManager.getAsync("", "a key"), 10 seconds) must equal (None)
+    }
 		"implement put method" in {
 			cacheManager.put("", "a key", "a value")
 			cacheManager.get("", "a key") must equal (Some("a value"))
