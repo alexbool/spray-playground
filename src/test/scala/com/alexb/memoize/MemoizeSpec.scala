c97fae5f30a246c103d4fdaf5583b7c646ea6738
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-12-07 21:21:30 +0400

    Fix compilation warnings

--- src/test/scala/com/alexb/memoize/MemoizeSpec.scala
+++ src/test/scala/com/alexb/memoize/MemoizeSpec.scala
@@ -16,42 +16,42 @@ class MemoizeSpec extends WordSpec with MustMatchers with BeforeAndAfterEach wit
 	"Memoize function" must {
 		"cache no-arg functions" in {
       val m = mockFunction[Int]
-      m.expects().returns(1).once
+      m.expects().returns(1).once()
 			val memoized = memoize(m)
 			memoized() must equal(1)
 			memoized() must equal(1)
 		}
 		"cache one-arg functions" in {
       val m = mockFunction[Int, Int]
-      m.expects(*).returns(2).once
+      m.expects(*).returns(2).once()
       val memoized = memoize(m)
 			memoized(1) must equal(2)
 			memoized(1) must equal(2)
 		}
 		"cache two-arg functions" in {
       val m = mockFunction[Int, Int, Int]
-      m.expects(*, *).returns(3).once
+      m.expects(*, *).returns(3).once()
       val memoized = memoize(m)
 			memoized(1, 2) must equal(3)
 			memoized(1, 2) must equal(3)
 		}
 		"cache three-arg functions" in {
       val m = mockFunction[Int, Int, Int, Int]
-      m.expects(*, *, *).returns(3).once
+      m.expects(*, *, *).returns(3).once()
       val memoized = memoize(m)
 			memoized(1, 1, 1) must equal(3)
 			memoized(1, 1, 1) must equal(3)
 		}
 		"cache four-arg functions" in {
       val m = mockFunction[Int, Int, Int, Int, Int]
-      m.expects(*, *, *, *).returns(4).once
+      m.expects(*, *, *, *).returns(4).once()
       val memoized = memoize(m)
 			memoized(1, 1, 1, 1) must equal(4)
 			memoized(1, 1, 1, 1) must equal(4)
 		}
 		"cache no-arg functions with cache name and key" in {
       val m = mockFunction[Int]
-      m.expects().returns(1).once
+      m.expects().returns(1).once()
 			def memoized = memoize(specialCacheName, "key", m)
 			memoized() must equal(1)
 			memoized() must equal(1)
