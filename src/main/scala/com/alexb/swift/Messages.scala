b2105c8e1f6e5d8c98ad3e10cb799cdd791519e8
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-12-11 18:09:43 +0400

    Work in propgress (#14)

--- src/main/scala/com/alexb/swift/Messages.scala
+++ src/main/scala/com/alexb/swift/Messages.scala
@@ -1,4 +1,11 @@
 package com.alexb.swift
 
 case object ListContainers
-case class CreateContainer(name: String)
+
+case class ListObjects(container: String)
+case class CreateContainer(container: String)
+case class DeleteContainer(container: String)
+
+case class GetObject(container: String, name: String)
+case class PutObject(container: String, name: String, data: Array[Byte])
+case class DeleteObject(container: String, name: String)
--- src/main/scala/com/alexb/swift/SwiftClient.scala
+++ src/main/scala/com/alexb/swift/SwiftClient.scala
@@ -9,14 +9,16 @@ import HttpConduit._
 case class SwiftCredentials(user: String, key: String)
 
 class SwiftClient(authUrl: String, credentials: SwiftCredentials, httpClient: ActorRef) extends Actor with ActorLogging {
+  private val authConduit = context.system.actorOf(Props(new HttpConduit(httpClient, authUrl, 80)))
+
   private val authPipeline =
     addHeader("X-Auth-User", credentials.user) ~>
     addHeader("X-Auth-Key", credentials.key) ~>
     sendReceive(authConduit)
-  private val authConduit = context.system.actorOf(Props(new HttpConduit(httpClient, authUrl, 80)))
 
   private var authToken: Option[String] = None
   private var storageUrl: Option[String] = None
+
   private def getToken = authToken match {
     case Some(token) => token
     case None => {
@@ -26,8 +28,9 @@ class SwiftClient(authUrl: String, credentials: SwiftCredentials, httpClient: Ac
   }
 
   private def authenticate {
-    val f = Get("/v1.0") ~> authPipeline
-    val res = Await.result(f, 10 seconds)
+    val res = Await.result(Get("/v1.0") ~> authPipeline, 10 seconds)
+    authToken = res.headers.find(_.name == "X-Auth-Token").map(_.value)
+    storageUrl = res.headers.find(_.name == "X-Storage-Url").map(_.value)
   }
 
   def receive = ???
