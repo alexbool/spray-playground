4f58e81b95263012d0608dbb3e83ed13b0d17773
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-12-13 20:59:23 +0400

    Add mkurlJson and use itl (#14)

--- src/main/scala/com/alexb/swift/SwiftApiUtils.scala
+++ src/main/scala/com/alexb/swift/SwiftApiUtils.scala
@@ -9,5 +9,9 @@ private[swift] trait SwiftApiUtils {
   def mkUrl(root: String, account: String, container: String)                   = s"$root/$account/$container"
   def mkUrl(root: String, account: String, container: String, `object`: String) = s"$root/$account/$container/${`object`}"
 
+  def mkUrlJson(root: String, account: String)                                      = s"${mkUrl(root, account)}$formatJson"
+  def mkUrlJson(root: String, account: String, container: String)                   = s"${mkUrl(root, account, container)}$formatJson"
+  def mkUrlJson(root: String, account: String, container: String, `object`: String) = s"${mkUrl(root, account, container, `object`)}$formatJson"
+
   def authHeader(token: String) = addHeader("X-Storage-Token", token)
 }
