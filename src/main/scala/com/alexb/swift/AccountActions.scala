73729e1d2a73142489318f05270d77063319ccc5
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-12-13 20:54:00 +0400

    Rename to mkUrl (#14)

--- src/main/scala/com/alexb/swift/AccountActions.scala
+++ src/main/scala/com/alexb/swift/AccountActions.scala
@@ -8,7 +8,7 @@ case class Container(name: String, count: Int, bytes: Long)
 
 private[swift] trait AccountActions extends SwiftApiUtils with SwiftApiMarshallers {
   def listContainers(rootPath: String, account: String, token: String, httpConduit: ActorRef)(implicit ctx: ExecutionContext) =
-    Get(accountUrl(rootPath, account)) ~> (
+    Get(mkUrl(rootPath, account)) ~> (
       authHeader(token) ~>
       sendReceive(httpConduit) ~>
       unmarshal[Seq[Container]]
--- src/main/scala/com/alexb/swift/SwiftApiUtils.scala
+++ src/main/scala/com/alexb/swift/SwiftApiUtils.scala
@@ -3,9 +3,11 @@ package com.alexb.swift
 import spray.client.HttpConduit._
 
 private[swift] trait SwiftApiUtils {
-  def accountUrl(root: String, account: String)                                     = s"$root/$account"
-  def containerUrl(root: String, account: String, container: String)                = s"$root/$account/$container"
-  def objectUrl(root: String, account: String, container: String, `object`: String) = s"$root/$account/$container/${`object`}"
+  private val formatJson = "?format=json"
+
+  def mkUrl(root: String, account: String)                                      = s"$root/$account"
+  def mkUrl(root: String, account: String, container: String)                   = s"$root/$account/$container"
+  def mkUrl(root: String, account: String, container: String, `object`: String) = s"$root/$account/$container/${`object`}"
 
   def authHeader(token: String) = addHeader("X-Storage-Token", token)
 }
