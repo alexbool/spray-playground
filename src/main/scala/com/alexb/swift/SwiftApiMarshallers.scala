852108b023cdef5a282df54c5ac887b2d9cff2d0
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-12-14 12:35:44 +0400

    Rename (#14)

--- src/main/scala/com/alexb/swift/SwiftApiMarshallers.scala
+++ src/main/scala/com/alexb/swift/SwiftApiMarshallers.scala
@@ -7,7 +7,7 @@ import org.joda.time.Instant
 private[swift] trait SwiftApiMarshallers extends SprayJsonSupport with DefaultJsonProtocol {
   implicit val containerFormat = jsonFormat3(Container)
 
-  implicit def objectFormat = new RootJsonFormat[ObjectMetadata] {
+  implicit def objectMetadataFormat = new RootJsonFormat[ObjectMetadata] {
     def read(json: JsValue) =
       json.asJsObject.getFields("name", "hash", "bytes", "content_type", "last_modified") match {
         case Seq(JsString(name), JsString(hash), JsNumber(bytes), JsString(contentType), JsString(lastModified)) =>
