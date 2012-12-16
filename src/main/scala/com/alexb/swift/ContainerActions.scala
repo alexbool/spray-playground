79d8f19298d1c78dd6350844adc1fa79769ec4e7
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-12-16 19:18:37 +0400

    Fmt (#14)

--- src/main/scala/com/alexb/swift/ContainerActions.scala
+++ src/main/scala/com/alexb/swift/ContainerActions.scala
@@ -6,14 +6,22 @@ import spray.client.HttpConduit._
 import spray.http.StatusCodes
 
 private[swift] trait ContainerActions extends SwiftApiUtils with SwiftApiMarshallers {
-  def listObjects(rootPath: String, container: String, token: String, httpConduit: ActorRef)(implicit ctx: ExecutionContext) =
+  def listObjects(rootPath: String,
+                  container: String,
+                  token: String,
+                  httpConduit: ActorRef)
+                 (implicit ctx: ExecutionContext) =
     Get(mkUrlJson(rootPath, container)) ~> (
       authHeader(token) ~>
       sendReceive(httpConduit) ~>
       unmarshal[Seq[ObjectMetadata]]
     )
 
-  def createContainer(rootPath: String, container: String, token: String, httpConduit: ActorRef)(implicit ctx: ExecutionContext) =
+  def createContainer(rootPath: String,
+                      container: String,
+                      token: String,
+                      httpConduit: ActorRef)
+                     (implicit ctx: ExecutionContext) =
     Put(mkUrl(rootPath, container)) ~> (
       authHeader(token) ~>
       sendReceive(httpConduit)
@@ -21,11 +29,17 @@ private[swift] trait ContainerActions extends SwiftApiUtils with SwiftApiMarshal
       CreateContainerResult(resp.status.isSuccess, resp.status == StatusCodes.Accepted)
     }
 
-  def deleteContainer(rootPath: String, container: String, token: String, httpConduit: ActorRef)(implicit ctx: ExecutionContext) =
+  def deleteContainer(rootPath: String,
+                      container: String,
+                      token: String,
+                      httpConduit: ActorRef)
+                     (implicit ctx: ExecutionContext) =
     Delete(mkUrl(rootPath, container)) ~> (
       authHeader(token) ~>
       sendReceive(httpConduit)
     ) map { resp =>
-      DeleteContainerResult(resp.status.isSuccess || resp.status == StatusCodes.NotFound, resp.status == StatusCodes.NotFound)
+      DeleteContainerResult(
+        resp.status.isSuccess || resp.status == StatusCodes.NotFound,
+        resp.status == StatusCodes.NotFound)
     }
 }
