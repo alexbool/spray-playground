d458f5c23d241e2af53ee89f0b097ae4d39bb1de
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-12-22 20:29:55 +0400

    Add deserialization method

--- src/main/scala/com/alexb/calculator/CalculatorMarshallers.scala
+++ src/main/scala/com/alexb/calculator/CalculatorMarshallers.scala
@@ -10,7 +10,17 @@ trait CalculatorMarshallers extends DefaultJsonProtocol {
       case t: SuccessResult => JsObject("success" -> JsBoolean(t.success), "result" -> JsNumber(t.result))
       case t: ErrorResult   => JsObject("success" -> JsBoolean(t.success), "reason" -> JsString(t.reason))
     }
-    def read(value: JsValue) = deserializationError("Unsupported operation", null)
+    def read(value: JsValue) = value.asJsObject.getFields("success") match {
+      case Seq(JsBoolean(true)) => value.asJsObject.getFields("result") match {
+        case Seq(JsNumber(result)) => SuccessResult(result.toDouble)
+        case _                     => deserializationError("Unknown format")
+      }
+      case Seq(JsBoolean(false)) => value.asJsObject.getFields("reason") match {
+        case Seq(JsString(reason)) => ErrorResult(reason)
+        case _                     => deserializationError("Unknown format")
+      }
+      case _ => deserializationError("Unknown format")
+    }
   }
 }
 
