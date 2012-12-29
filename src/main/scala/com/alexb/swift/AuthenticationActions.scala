2cafc313684d0dbc5a14114fe3e725669a8376cf
Alexander Bulaev <aleks.bulaev@gmail.com>
2012-12-29 23:29:40 +0400

    Work in progress (#20)

--- src/main/scala/com/alexb/swift/AuthenticationActions.scala
+++ src/main/scala/com/alexb/swift/AuthenticationActions.scala
@@ -1,21 +1,33 @@
 package com.alexb.swift
 
+import spray.client.HttpConduit
 import spray.client.HttpConduit._
-import akka.actor.{ActorRef, ActorLogging}
+import akka.actor.{Props, Actor, ActorRef, ActorLogging}
 import scala.concurrent.ExecutionContext
 
 trait AuthenticationActions {
-  this: ActorLogging =>
+  this: Actor with ActorLogging =>
 
-  def authenticate(conduit: ActorRef, msg: Authenticate)(implicit ctx: ExecutionContext) = {
-    log.debug(s"About to make authentication request: $msg")
-    (Get("/v1.0") ~> authPipeline(conduit, msg.credentials))
+  def authenticate(httpClient: ActorRef,
+                   credentials: SwiftCredentials,
+                   host: String,
+                   port: Int,
+                   sslEnabled: Boolean)(implicit ctx: ExecutionContext) = {
+    val authConduit = context.actorOf(
+      props = Props(new HttpConduit(httpClient, host, port, sslEnabled)),
+      name = "auth-http-conduit")
+    log.debug(s"About to make authentication request: $credentials")
+    val auth = (Get("/v1.0") ~> authPipeline(authConduit, credentials))
       .map { resp =>
       log.debug(s"Recieved authentication response: $resp")
       AuthenticationResult(
         resp.headers.find(_.is("x-auth-token")).map(_.value).get,
         resp.headers.find(_.is("x-storage-url")).map(h => Url(h.value)).get)
     }
+    auth onComplete { r =>
+      context.stop(authConduit)
+    }
+    auth
   }
 
   private def authPipeline(conduit: ActorRef, credentials: SwiftCredentials) =
