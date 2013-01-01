32a9f31ddc641a5b7f707620f6504a4b1e538988
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-01-02 02:05:50 +0400

    Basic left join implementation (#22)

--- src/main/scala/com/alexb/join/Join.scala
+++ src/main/scala/com/alexb/join/Join.scala
@@ -10,4 +10,11 @@ object Join {
 
   def innerJoin[A, B, K](left: Iterable[A], right: Iterable[B], leftKey: A => K, rightKey: B => K) =
     join(left, right, leftKey, rightKey)
+
+  def leftJoin[A, B, K](left: Iterable[A], right: Iterable[B], leftKey: A => K, rightKey: B => K) = {
+    val rightMap = right.map(e => (rightKey(e), e)).toMap
+    for {
+      leftElem <- left.map(e => (leftKey(e), e))
+    } yield (leftElem._2, rightMap.get(leftElem._1))
+  }
 }
--- src/test/scala/com/alexb/join/JoinSpec.scala
+++ src/test/scala/com/alexb/join/JoinSpec.scala
@@ -10,5 +10,10 @@ class JoinSpec extends WordSpec with MustMatchers {
       val right = List(2, 3)
       Join.join(left, right, identity[Int], identity[Int]) must be (Iterable((2, 2), (3, 3)))
     }
+    "do left joins" in {
+      val left = List(1, 2, 3)
+      val right = List(2, 3)
+      Join.leftJoin(left, right, identity[Int], identity[Int]) must be (Iterable((1, None), (2, Some(2)), (3, Some(3))))
+    }
   }
 }
