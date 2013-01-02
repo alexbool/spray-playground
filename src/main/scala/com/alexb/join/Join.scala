104d66b6cf070f8039e8f0ab0704fdd0d19521cb
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-01-02 20:56:48 +0400

    Add one-to-one joins (#22)

--- src/main/scala/com/alexb/join/Join.scala
+++ src/main/scala/com/alexb/join/Join.scala
@@ -26,6 +26,23 @@ object Join {
     })
   }
 
+  def oneToOneJoin[A, B, K](left: Iterable[A], right: Iterable[B], leftKey: A => K, rightKey: B => K) = {
+    val rightMap = right.map(e => (rightKey(e), e)).toMap
+    for {
+      leftElem <- left.map(e => (leftKey(e), e)) if rightMap.contains(leftElem._1)
+    } yield (leftElem._2, rightMap.get(leftElem._1).get)
+  }
+
+  def oneToOneInnerJoin[A, B, K](left: Iterable[A], right: Iterable[B], leftKey: A => K, rightKey: B => K) =
+    join(left, right, leftKey, rightKey)
+
+  def oneToOneLeftJoin[A, B, K](left: Iterable[A], right: Iterable[B], leftKey: A => K, rightKey: B => K) = {
+    val rightMap = right.map(e => (rightKey(e), e)).toMap
+    for {
+      leftElem <- left.map(e => (leftKey(e), e))
+    } yield (leftElem._2, rightMap.get(leftElem._1))
+  }
+
   private def iterableToMultiMap[A, B](collection: Iterable[(A, B)]) =
     collection.foldLeft(new mutable.HashMap[A, mutable.ListBuffer[B]]) { (acc, pair) =>
       acc.get(pair._1) match {
@@ -43,5 +60,8 @@ object Join {
     def join[B, K](right: Iterable[B], leftKey: A => K, rightKey: B => K) = Join.join(left, right, leftKey, rightKey)
     def innerJoin[B, K](right: Iterable[B], leftKey: A => K, rightKey: B => K) = join(right, leftKey, rightKey)
     def leftJoin[B, K](right: Iterable[B], leftKey: A => K, rightKey: B => K) = Join.leftJoin(left, right, leftKey, rightKey)
+    def oneToOneJoin[B, K](right: Iterable[B], leftKey: A => K, rightKey: B => K) = Join.oneToOneJoin(left, right, leftKey, rightKey)
+    def oneToOneInnerJoin[B, K](right: Iterable[B], leftKey: A => K, rightKey: B => K) = oneToOneJoin(right, leftKey, rightKey)
+    def oneToOneLeftJoin[B, K](right: Iterable[B], leftKey: A => K, rightKey: B => K) = Join.oneToOneLeftJoin(left, right, leftKey, rightKey)
   }
 }
--- src/test/scala/com/alexb/join/JoinSpec.scala
+++ src/test/scala/com/alexb/join/JoinSpec.scala
@@ -22,5 +22,21 @@ class JoinSpec extends WordSpec with MustMatchers {
     "do one-to-many left joins with implicit conversion" in {
       List(1, 2, 3).leftJoin(List(2, 2, 3), identity[Int], identity[Int]) must be (Iterable((1, Seq()), (2, Seq(2, 2)), (3, Seq(3))))
     }
+    "do one-to-one inner joins" in {
+      val left = List(1, 2, 3)
+      val right = List(2, 3)
+      Join.oneToOneJoin(left, right, identity[Int], identity[Int]) must be (Iterable((2, 2), (3, 3)))
+    }
+    "do one-to-one inner joins with implicit conversion" in {
+      List(1, 2, 3).oneToOneJoin(List(2, 3), identity[Int], identity[Int]) must be (Iterable((2, 2), (3, 3)))
+    }
+    "do one-to-one left joins" in {
+      val left = List(1, 2, 3)
+      val right = List(2, 3)
+      Join.oneToOneLeftJoin(left, right, identity[Int], identity[Int]) must be (Iterable((1, None), (2, Some(2)), (3, Some(3))))
+    }
+    "do one-to-one left joins with implicit conversion" in {
+      List(1, 2, 3).oneToOneLeftJoin(List(2, 3), identity[Int], identity[Int]) must be (Iterable((1, None), (2, Some(2)), (3, Some(3))))
+    }
   }
 }
