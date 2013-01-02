d47e19c4a13648cb29be30e9074100efc999e674
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-01-02 13:59:06 +0400

    Correct joins, as in RDBMS'es (#22)

--- src/main/scala/com/alexb/join/Join.scala
+++ src/main/scala/com/alexb/join/Join.scala
@@ -1,22 +1,45 @@
 package com.alexb.join
 
+import scala.collection.mutable
+import scala.collection.mutable.ListBuffer
+
 object Join {
   def join[A, B, K](left: Iterable[A], right: Iterable[B], leftKey: A => K, rightKey: B => K) = {
-    val rightMap = right.map(e => (rightKey(e), e)).toMap
-    for {
-      leftElem <- left.map(e => (leftKey(e), e)) if rightMap.contains(leftElem._1)
-    } yield (leftElem._2, rightMap.get(leftElem._1).get)
+    val rightMultiMap = iterableToMultiMap(right.map(e => (rightKey(e), e)))
+    (for {
+      leftElem <- left.map(e => (leftKey(e), e)) if rightMultiMap.contains(leftElem._1)
+    } yield (leftElem._2, rightMultiMap.get(leftElem._1).get))
+    .flatMap({ e => e._2.map(s => (e._1, s)) })
   }
 
   def innerJoin[A, B, K](left: Iterable[A], right: Iterable[B], leftKey: A => K, rightKey: B => K) =
     join(left, right, leftKey, rightKey)
 
   def leftJoin[A, B, K](left: Iterable[A], right: Iterable[B], leftKey: A => K, rightKey: B => K) = {
-    val rightMap = right.map(e => (rightKey(e), e)).toMap
-    for {
+    val rightMultiMap = iterableToMultiMap(right.map(e => (rightKey(e), e)))
+    (for {
       leftElem <- left.map(e => (leftKey(e), e))
-    } yield (leftElem._2, rightMap.get(leftElem._1))
+    } yield (leftElem._2, rightMultiMap.get(leftElem._1)))
+    .flatMap({ e =>
+      e._2 match {
+        case None => List((e._1, None))
+        case Some(list) => list.map(s => (e._1, Some(s)))
+      }
+    })
   }
+
+  private def iterableToMultiMap[A, B](collection: Iterable[(A, B)]) =
+    collection.foldLeft(new mutable.HashMap[A, mutable.ListBuffer[B]]) { (acc, pair) =>
+      acc.get(pair._1) match {
+        case None =>
+          val list = ListBuffer[B]()
+          list += pair._2
+          acc.put(pair._1, list)
+        case Some(list) =>
+          list += pair._2
+      }
+      acc
+    }
 
   implicit class Joinable[A](left: Iterable[A]) {
     def join[B, K](right: Iterable[B], leftKey: A => K, rightKey: B => K) = Join.join(left, right, leftKey, rightKey)
--- src/test/scala/com/alexb/join/JoinSpec.scala
+++ src/test/scala/com/alexb/join/JoinSpec.scala
@@ -8,19 +8,19 @@ class JoinSpec extends WordSpec with MustMatchers {
   "Join class" must {
     "do inner joins" in {
       val left = List(1, 2, 3)
-      val right = List(2, 3)
-      Join.join(left, right, identity[Int], identity[Int]) must be (Iterable((2, 2), (3, 3)))
+      val right = List(2, 2, 3)
+      Join.join(left, right, identity[Int], identity[Int]) must be (Iterable((2, 2), (2, 2), (3, 3)))
     }
     "do inner joins with implicit conversion" in {
       List(1, 2, 3).join(List(2, 3), identity[Int], identity[Int]) must be (Iterable((2, 2), (3, 3)))
     }
     "do left joins" in {
       val left = List(1, 2, 3)
-      val right = List(2, 3)
-      Join.leftJoin(left, right, identity[Int], identity[Int]) must be (Iterable((1, None), (2, Some(2)), (3, Some(3))))
+      val right = List(2, 2, 3)
+      Join.leftJoin(left, right, identity[Int], identity[Int]) must be (Iterable((1, None), (2, Some(2)), (2, Some(2)), (3, Some(3))))
     }
     "do left joins with implicit conversion" in {
-      List(1, 2, 3).leftJoin(List(2, 3), identity[Int], identity[Int]) must be (Iterable((1, None), (2, Some(2)), (3, Some(3))))
+      List(1, 2, 3).leftJoin(List(2, 2, 3), identity[Int], identity[Int]) must be (Iterable((1, None), (2, Some(2)), (2, Some(2)), (3, Some(3))))
     }
   }
 }
