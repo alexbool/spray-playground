7236111902ed1fc80f775bf43314aadb3f8c7d50
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-01-02 19:40:44 +0400

    More OOP-like design of join API (#22)

--- src/main/scala/com/alexb/join/Join.scala
+++ src/main/scala/com/alexb/join/Join.scala
@@ -1,15 +1,13 @@
 package com.alexb.join
 
 import scala.collection.mutable
-import scala.collection.mutable.ListBuffer
 
 object Join {
   def join[A, B, K](left: Iterable[A], right: Iterable[B], leftKey: A => K, rightKey: B => K) = {
     val rightMultiMap = iterableToMultiMap(right.map(e => (rightKey(e), e)))
-    (for {
+    for {
       leftElem <- left.map(e => (leftKey(e), e)) if rightMultiMap.contains(leftElem._1)
-    } yield (leftElem._2, rightMultiMap.get(leftElem._1).get))
-    .flatMap({ e => e._2.map(s => (e._1, s)) })
+    } yield (leftElem._2, rightMultiMap.get(leftElem._1).get.to[Seq])
   }
 
   def innerJoin[A, B, K](left: Iterable[A], right: Iterable[B], leftKey: A => K, rightKey: B => K) =
@@ -20,10 +18,10 @@ object Join {
     (for {
       leftElem <- left.map(e => (leftKey(e), e))
     } yield (leftElem._2, rightMultiMap.get(leftElem._1)))
-    .flatMap({ e =>
+    .map({ e =>
       e._2 match {
-        case None => List((e._1, None))
-        case Some(list) => list.map(s => (e._1, Some(s)))
+        case None => (e._1, Seq[B]())
+        case Some(list) => (e._1, list.to[Seq])
       }
     })
   }
@@ -32,7 +30,7 @@ object Join {
     collection.foldLeft(new mutable.HashMap[A, mutable.ListBuffer[B]]) { (acc, pair) =>
       acc.get(pair._1) match {
         case None =>
-          val list = ListBuffer[B]()
+          val list = mutable.ListBuffer[B]()
           list += pair._2
           acc.put(pair._1, list)
         case Some(list) =>
--- src/test/scala/com/alexb/join/JoinSpec.scala
+++ src/test/scala/com/alexb/join/JoinSpec.scala
@@ -6,21 +6,21 @@ import Join._
 
 class JoinSpec extends WordSpec with MustMatchers {
   "Join class" must {
-    "do inner joins" in {
+    "do one-to-many inner joins" in {
       val left = List(1, 2, 3)
       val right = List(2, 2, 3)
-      Join.join(left, right, identity[Int], identity[Int]) must be (Iterable((2, 2), (2, 2), (3, 3)))
+      Join.join(left, right, identity[Int], identity[Int]) must be (Iterable((2, Seq(2, 2)), (3, Seq(3))))
     }
-    "do inner joins with implicit conversion" in {
-      List(1, 2, 3).join(List(2, 3), identity[Int], identity[Int]) must be (Iterable((2, 2), (3, 3)))
+    "do one-to-many inner joins with implicit conversion" in {
+      List(1, 2, 3).join(List(2, 2, 3), identity[Int], identity[Int]) must be (Iterable((2, Seq(2, 2)), (3, Seq(3))))
     }
-    "do left joins" in {
+    "do one-to-many left joins" in {
       val left = List(1, 2, 3)
       val right = List(2, 2, 3)
-      Join.leftJoin(left, right, identity[Int], identity[Int]) must be (Iterable((1, None), (2, Some(2)), (2, Some(2)), (3, Some(3))))
+      Join.leftJoin(left, right, identity[Int], identity[Int]) must be (Iterable((1, Seq()), (2, Seq(2, 2)), (3, Seq(3))))
     }
-    "do left joins with implicit conversion" in {
-      List(1, 2, 3).leftJoin(List(2, 2, 3), identity[Int], identity[Int]) must be (Iterable((1, None), (2, Some(2)), (2, Some(2)), (3, Some(3))))
+    "do one-to-many left joins with implicit conversion" in {
+      List(1, 2, 3).leftJoin(List(2, 2, 3), identity[Int], identity[Int]) must be (Iterable((1, Seq()), (2, Seq(2, 2)), (3, Seq(3))))
     }
   }
 }
