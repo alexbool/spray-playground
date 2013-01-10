6bfd3f7afadc230edaff56ec56a6c4d0ada0e21f
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-01-10 20:07:52 +0400

    Rename

--- src/main/scala/com/alexb/swift/ContainerActions.scala
+++ src/main/scala/com/alexb/swift/ContainerActions.scala
@@ -9,22 +9,22 @@ private[swift] trait ContainerActions extends SwiftApiUtils with SwiftMarshaller
   def listObjects(rootPath: String,
                   container: String,
                   token: String,
-                  httpConduit: ActorRef)
+                  httpClient: ActorRef)
                  (implicit ctx: ExecutionContext) =
     Get(mkUrlJson(rootPath, container)) ~> (
       authHeader(token) ~>
-      sendReceive(httpConduit) ~>
+      sendReceive(httpClient) ~>
       unmarshal[Seq[ObjectMetadata]]
     )
 
   def createContainer(rootPath: String,
                       container: String,
                       token: String,
-                      httpConduit: ActorRef)
+                      httpClient: ActorRef)
                      (implicit ctx: ExecutionContext) =
     Put(mkUrl(rootPath, container)) ~> (
       authHeader(token) ~>
-      sendReceive(httpConduit)
+      sendReceive(httpClient)
     ) map { resp =>
       CreateContainerResult(resp.status.isSuccess, resp.status == StatusCodes.Accepted)
     }
@@ -32,11 +32,11 @@ private[swift] trait ContainerActions extends SwiftApiUtils with SwiftMarshaller
   def deleteContainer(rootPath: String,
                       container: String,
                       token: String,
-                      httpConduit: ActorRef)
+                      httpClient: ActorRef)
                      (implicit ctx: ExecutionContext) =
     Delete(mkUrl(rootPath, container)) ~> (
       authHeader(token) ~>
-      sendReceive(httpConduit)
+      sendReceive(httpClient)
     ) map { resp =>
       DeleteContainerResult(
         resp.status.isSuccess || resp.status == StatusCodes.NotFound,
