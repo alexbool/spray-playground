b35daa916828ba2e708dcca690d3b01fe3116d03
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-01-12 00:33:00 +0400

    Add scaladoc

--- src/main/scala/com/alexb/join/Join.scala
+++ src/main/scala/com/alexb/join/Join.scala
@@ -1,6 +1,27 @@
 package com.alexb.join
 
+/**
+ * This object contains several methods for SQL-like joining collections.
+ *
+ * @author Alexander Bulaev
+ */
 object Join {
+
+  /**
+   * Joins elements of right collection to the left collection, following the rule:
+   * `leftKey(leftElem) == rightKey(rightElem)`.
+   *
+   * Elements from left collection with no matches in right collection are skipped.
+   *
+   * @param left     left collection
+   * @param right    right collection
+   * @param leftKey  left collection key projection
+   * @param rightKey right collection key projection
+   * @tparam A       left collection type
+   * @tparam B       right collection type
+   * @tparam K       key type
+   * @return         joined collection
+   */
   def join[A, B, K](left: Iterable[A], right: Iterable[B], leftKey: A => K, rightKey: B => K) = {
     val rightGrouped = right.groupBy(rightKey(_))
     for {
@@ -8,9 +29,36 @@ object Join {
     } yield (leftElem._2, rightGrouped.get(leftElem._1).get.to[Seq])
   }
 
+  /**
+   * Alias for [[com.alexb.join.Join.join]]
+   *
+   * @param left     left collection
+   * @param right    right collection
+   * @param leftKey  left collection key projection
+   * @param rightKey right collection key projection
+   * @tparam A       left collection type
+   * @tparam B       right collection type
+   * @tparam K       key type
+   * @return         joined collection
+   */
   def innerJoin[A, B, K](left: Iterable[A], right: Iterable[B], leftKey: A => K, rightKey: B => K) =
     join(left, right, leftKey, rightKey)
 
+  /**
+   * Joins elements of right collection to the left collection, following the rule:
+   * `leftKey(leftElem) == rightKey(rightElem)`.
+   *
+   * No elements from left collection are skipped, even if they have no matches in the right one.
+   *
+   * @param left     left collection
+   * @param right    right collection
+   * @param leftKey  left collection key projection
+   * @param rightKey right collection key projection
+   * @tparam A       left collection type
+   * @tparam B       right collection type
+   * @tparam K       key type
+   * @return         joined collection
+   */
   def leftJoin[A, B, K](left: Iterable[A], right: Iterable[B], leftKey: A => K, rightKey: B => K) = {
     val rightGrouped = right.groupBy(rightKey(_))
     (for {
@@ -24,6 +72,24 @@ object Join {
     })
   }
 
+  /**
+   * Joins elements of right collection to the left collection, following the rule:
+   * `leftKey(leftElem) == rightKey(rightElem)`.
+   *
+   * If two or more elements from the right collection are matched against an element from the left collection, only the
+   * first one is taken.
+   *
+   * Elements from left collection with no matches in right collection are skipped.
+   *
+   * @param left     left collection
+   * @param right    right collection
+   * @param leftKey  left collection key projection
+   * @param rightKey right collection key projection
+   * @tparam A       left collection type
+   * @tparam B       right collection type
+   * @tparam K       key type
+   * @return         joined collection
+   */
   def oneToOneJoin[A, B, K](left: Iterable[A], right: Iterable[B], leftKey: A => K, rightKey: B => K) = {
     val rightMap = right.map(e => (rightKey(e), e)).toMap
     for {
@@ -31,9 +97,39 @@ object Join {
     } yield (leftElem._2, rightMap.get(leftElem._1).get)
   }
 
+  /**
+   * Alias for [[com.alexb.join.Join.oneToOneJoin]]
+   *
+   * @param left     left collection
+   * @param right    right collection
+   * @param leftKey  left collection key projection
+   * @param rightKey right collection key projection
+   * @tparam A       left collection type
+   * @tparam B       right collection type
+   * @tparam K       key type
+   * @return         joined collection
+   */
   def oneToOneInnerJoin[A, B, K](left: Iterable[A], right: Iterable[B], leftKey: A => K, rightKey: B => K) =
     join(left, right, leftKey, rightKey)
 
+  /**
+   * Joins elements of right collection to the left collection, following the rule:
+   * `leftKey(leftElem) == rightKey(rightElem)`.
+   *
+   * If two or more elements from the right collection are matched against an element from the left collection, only the
+   * first one is taken.
+   *
+   * No elements from left collection are skipped, even if they have no matches in the right one.
+   *
+   * @param left     left collection
+   * @param right    right collection
+   * @param leftKey  left collection key projection
+   * @param rightKey right collection key projection
+   * @tparam A       left collection type
+   * @tparam B       right collection type
+   * @tparam K       key type
+   * @return         joined collection
+   */
   def oneToOneLeftJoin[A, B, K](left: Iterable[A], right: Iterable[B], leftKey: A => K, rightKey: B => K) = {
     val rightMap = right.map(e => (rightKey(e), e)).toMap
     for {
