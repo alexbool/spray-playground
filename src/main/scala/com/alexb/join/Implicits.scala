330dbb94340f9a47cdf43d765e4a748042727625
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-01-12 14:18:59 +0400

    Use CanBuildFrom in Implicits

--- src/main/scala/com/alexb/join/Implicits.scala
+++ src/main/scala/com/alexb/join/Implicits.scala
@@ -1,12 +1,31 @@
 package com.alexb.join
 
 object Implicits {
-  implicit class Joinable[A](left: Iterable[A]) {
-    def join[B, K](right: Iterable[B], leftKey: A => K, rightKey: B => K) = Join.join(left, right, leftKey, rightKey)
-    def innerJoin[B, K](right: Iterable[B], leftKey: A => K, rightKey: B => K) = join(right, leftKey, rightKey)
-    def leftJoin[B, K](right: Iterable[B], leftKey: A => K, rightKey: B => K) = Join.leftJoin(left, right, leftKey, rightKey)
-    def oneToOneJoin[B, K](right: Iterable[B], leftKey: A => K, rightKey: B => K) = Join.oneToOneJoin(left, right, leftKey, rightKey)
-    def oneToOneInnerJoin[B, K](right: Iterable[B], leftKey: A => K, rightKey: B => K) = oneToOneJoin(right, leftKey, rightKey)
-    def oneToOneLeftJoin[B, K](right: Iterable[B], leftKey: A => K, rightKey: B => K) = Join.oneToOneLeftJoin(left, right, leftKey, rightKey)
+  implicit class Joinable[A, Col[X] <: Iterable[X]](left: Col[A]) {
+    import Join.CBF
+
+    def join[B, K](right: Iterable[B], leftKey: A => K, rightKey: B => K)
+                  (implicit cbf: CBF[Col, (A, Seq[B])]): Col[(A, Seq[B])] =
+      Join.join(left, right, leftKey, rightKey)
+
+    def innerJoin[B, K](right: Iterable[B], leftKey: A => K, rightKey: B => K)
+                       (implicit cbf: CBF[Col, (A, Seq[B])]): Col[(A, Seq[B])] =
+      join(right, leftKey, rightKey)
+
+    def leftJoin[B, K](right: Iterable[B], leftKey: A => K, rightKey: B => K)
+                      (implicit cbf: CBF[Col, (A, Seq[B])]): Col[(A, Seq[B])] =
+      Join.leftJoin(left, right, leftKey, rightKey)
+
+    def oneToOneJoin[B, K](right: Iterable[B], leftKey: A => K, rightKey: B => K)
+                          (implicit cbf: CBF[Col, (A, B)]): Col[(A, B)] =
+      Join.oneToOneJoin(left, right, leftKey, rightKey)
+
+    def oneToOneInnerJoin[B, K](right: Iterable[B], leftKey: A => K, rightKey: B => K)
+                               (implicit cbf: CBF[Col, (A, B)]): Col[(A, B)] =
+      oneToOneJoin(right, leftKey, rightKey)
+
+    def oneToOneLeftJoin[B, K](right: Iterable[B], leftKey: A => K, rightKey: B => K)
+                              (implicit cbf: CBF[Col, (A, Option[B])]): Col[(A, Option[B])] =
+      Join.oneToOneLeftJoin(left, right, leftKey, rightKey)
   }
 }
--- src/test/scala/com/alexb/join/JoinSpec.scala
+++ src/test/scala/com/alexb/join/JoinSpec.scala
@@ -12,7 +12,7 @@ class JoinSpec extends WordSpec with MustMatchers {
       Join.join(left, right, identity[Int], identity[Int]) must be (List((2, Seq(2, 2)), (3, Seq(3))))
     }
     "do one-to-many inner joins with implicit conversion" in {
-      List(1, 2, 3).join(List(2, 2, 3), identity[Int], identity[Int]) must be (Iterable((2, Seq(2, 2)), (3, Seq(3))))
+      List(1, 2, 3).join(List(2, 2, 3), identity[Int], identity[Int]) must be (List((2, Seq(2, 2)), (3, Seq(3))))
     }
     "do one-to-many left joins" in {
       val left = List(1, 2, 3)
@@ -20,7 +20,7 @@ class JoinSpec extends WordSpec with MustMatchers {
       Join.leftJoin(left, right, identity[Int], identity[Int]) must be (List((1, Seq()), (2, Seq(2, 2)), (3, Seq(3))))
     }
     "do one-to-many left joins with implicit conversion" in {
-      List(1, 2, 3).leftJoin(List(2, 2, 3), identity[Int], identity[Int]) must be (Iterable((1, Seq()), (2, Seq(2, 2)), (3, Seq(3))))
+      List(1, 2, 3).leftJoin(List(2, 2, 3), identity[Int], identity[Int]) must be (List((1, Seq()), (2, Seq(2, 2)), (3, Seq(3))))
     }
     "do one-to-one inner joins" in {
       val left = List(1, 2, 3)
@@ -28,7 +28,7 @@ class JoinSpec extends WordSpec with MustMatchers {
       Join.oneToOneJoin(left, right, identity[Int], identity[Int]) must be (List((2, 2), (3, 3)))
     }
     "do one-to-one inner joins with implicit conversion" in {
-      List(1, 2, 3).oneToOneJoin(List(2, 3), identity[Int], identity[Int]) must be (Iterable((2, 2), (3, 3)))
+      List(1, 2, 3).oneToOneJoin(List(2, 3), identity[Int], identity[Int]) must be (List((2, 2), (3, 3)))
     }
     "do one-to-one left joins" in {
       val left = List(1, 2, 3)
@@ -36,7 +36,7 @@ class JoinSpec extends WordSpec with MustMatchers {
       Join.oneToOneLeftJoin(left, right, identity[Int], identity[Int]) must be (List((1, None), (2, Some(2)), (3, Some(3))))
     }
     "do one-to-one left joins with implicit conversion" in {
-      List(1, 2, 3).oneToOneLeftJoin(List(2, 3), identity[Int], identity[Int]) must be (Iterable((1, None), (2, Some(2)), (3, Some(3))))
+      List(1, 2, 3).oneToOneLeftJoin(List(2, 3), identity[Int], identity[Int]) must be (List((1, None), (2, Some(2)), (3, Some(3))))
     }
   }
 }
