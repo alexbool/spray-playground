52776b2b19b9e083044e7a68202ea4c492109893
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-01-21 22:35:01 +0400

    Fmt + extract method

--- src/main/scala/com/alexb/zk/ZkProperties.scala
+++ src/main/scala/com/alexb/zk/ZkProperties.scala
@@ -14,18 +14,18 @@ class ZkProperties(connectString: String, sessionTimeout: Int) extends Watcher w
 
   def process(event: WatchedEvent) {
     event.getType match {
-      case EventType.None if event.getState == KeeperState.SyncConnected =>
-        fetchProperties()
-      case EventType.NodeChildrenChanged =>
-        fetchProperties()
-      case EventType.NodeDataChanged =>
-        zk.getData(event.getPath, this, new PropertyCallback(properties, toKey(event.getPath)), null)
-      case EventType.NodeDeleted =>
-        properties -= toKey(event.getPath)
+      case EventType.None if event.getState == KeeperState.SyncConnected => fetchProperties()
+      case EventType.NodeChildrenChanged                                 => fetchProperties()
+      case EventType.NodeDataChanged                                     => fetchProperty()
+      case EventType.NodeDeleted                                         => properties -= toKey(event.getPath)
     }
 
     def fetchProperties() {
       zk.getChildren("/", this, new ChildrenCallback(zk, this, properties), null)
+    }
+
+    def fetchProperty() {
+      zk.getData(event.getPath, this, new PropertyCallback(properties, toKey(event.getPath)), null)
     }
 
     def toKey(path: String) = path.replace("/", "")
