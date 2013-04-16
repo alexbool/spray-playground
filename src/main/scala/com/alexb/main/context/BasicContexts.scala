48e54a5e77b355df5ad590b1e5af5b182c4d8dd3
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-04-16 22:46:44 +0400

    ExecutionContextContext :)

--- src/main/scala/com/alexb/main/context/BasicContexts.scala
+++ src/main/scala/com/alexb/main/context/BasicContexts.scala
@@ -4,6 +4,7 @@ package context
 import akka.actor.ActorSystem
 import com.typesafe.config.Config
 import com.alexb.memoize.CacheManager
+import scala.concurrent.ExecutionContext
 
 trait ActorSystemContext {
   def actorSystem: ActorSystem
@@ -13,9 +14,16 @@ trait Configuration {
   def config: Config
 }
 
-trait ActorSystemConfiguration extends Configuration {
-  this: ActorSystemContext =>
+trait ActorSystemConfiguration extends Configuration { this: ActorSystemContext =>
   def config = actorSystem.settings.config
+}
+
+trait ExecutionContextContext {
+  def ec: ExecutionContext
+}
+
+trait ExecutionContextFromActorSystemContext extends ExecutionContextContext { this: ActorSystemContext =>
+  def ec = actorSystem.dispatcher
 }
 
 trait Caching {
