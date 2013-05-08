244ddeb83070e13ceb1653b5d958668e9be5a9c1
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-05-08 15:42:36 +0400

    Simpler code

--- src/main/scala/com/alexb/main/Main.scala
+++ src/main/scala/com/alexb/main/Main.scala
@@ -11,14 +11,13 @@ import com.alexb.statics.StaticsModule
 import com.alexb.user.UserModule
 import context._
 import language.postfixOps
-import spray.routing.RoutingSettings
 
 object Main extends App with ActorSystemFromAppContext {
 
   // Initialize application context beans
   Context.initialize()
 
-  implicit lazy val system = Context.actorSystem
+  def system = Context.actorSystem
 
   // create the service instance, supplying all required dependencies
   class SprayPlaygroundActor extends Actor with ActorSystemContext with ActorSystemConfiguration
@@ -32,8 +31,6 @@ object Main extends App with ActorSystemFromAppContext {
     // the HttpService trait defines only one abstract member, which
     // connects the services environment to the enclosing actor or test
     def actorRefFactory: ActorRefFactory = context
-
-    implicit val rs: RoutingSettings = RoutingSettings.default(actorRefFactory)
 
     // this actor only runs our route, but you could add
     // other things here, like request stream processing
@@ -53,7 +50,7 @@ object Main extends App with ActorSystemFromAppContext {
   ///////////////////////////////////////////////////////////////////////////
 
   // start a new HTTP server on seleccted port with our service actor as the handler
-  IO(Http) ! Http.Bind(httpService,
+  IO(Http)(system) ! Http.Bind(httpService,
       interface = system.settings.config.getString("application.host"),
       port = system.settings.config.getInt("application.port"))
 }
