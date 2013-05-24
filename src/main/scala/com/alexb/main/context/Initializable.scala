d65589abef51bda857380ac797d433843be1aa15
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-05-24 23:15:47 +0400

    Fix initializable trait

--- src/main/scala/com/alexb/main/context/Initializable.scala
+++ src/main/scala/com/alexb/main/context/Initializable.scala
@@ -1,21 +1,23 @@
 package com.alexb.main.context
 
+import scala.collection.mutable.ListBuffer
+
 /**
  * A trait that delays all initialization of target object until the `initilaize` method is called.
  */
 trait Initializable extends DelayedInit {
   private var isInitialized = false
-  private var initBody: () => Unit = null
+  private val initBody = new ListBuffer[() => Unit]
 
   def delayedInit(body: => Unit) {
-    initBody = () => body
+    initBody += (() => body)
   }
 
   def initialize() {
     this.synchronized {
       if (!isInitialized) {
         isInitialized = true
-        initBody()
+        for (initCode <- initBody) initCode()
       }
     }
   }
