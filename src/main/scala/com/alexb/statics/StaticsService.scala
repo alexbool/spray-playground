142986944a4e3d58003bd21c046d1d912c62d2ab
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-05-29 21:33:12 +0400

    Use implicit conversion

--- src/main/scala/com/alexb/statics/StaticsService.scala
+++ src/main/scala/com/alexb/statics/StaticsService.scala
@@ -3,12 +3,15 @@ package com.alexb.statics
 import scala.concurrent.{ExecutionContext, Future}
 import spray.routing.Directives
 import spray.httpx.SprayJsonSupport
-import com.alexb.memoize.{CacheManager, Memoize}
+import com.alexb.memoize.CacheManager
+import com.alexb.memoize.Implicits._
 import com.alexb.main.context.{ActorSystemContext, Caching, MongoSupport}
 import com.alexb.main.HttpRouteContainer
 
 class StaticsService(countryRepository: CountryRepository)(implicit ec: ExecutionContext, cache: CacheManager)
-  extends Directives with SprayJsonSupport with StaticsMarshallers with Memoize with HttpRouteContainer {
+  extends Directives with SprayJsonSupport with StaticsMarshallers with HttpRouteContainer {
+
+  private val countries = Future(countryRepository.findCountries).memoizedAsync("statics", "countries")
 
   val route =
     pathPrefix("static") {
@@ -20,8 +23,6 @@ class StaticsService(countryRepository: CountryRepository)(implicit ec: Executio
         }
       }
     }
-
-  private val countries = memoizeAsyncValue("statics", "countries", Future(countryRepository.findCountries))
 }
 
 trait StaticsServiceContext {
