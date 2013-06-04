6d85169fd41e87c717e9c6639c8ef0ee0eb6b9ef
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-06-04 17:45:12 +0400

    Another test case

--- src/test/scala/com/alexb/protobuf/ProtobufSerializerSpec.scala
+++ src/test/scala/com/alexb/protobuf/ProtobufSerializerSpec.scala
@@ -11,6 +11,7 @@ class ProtobufSerializerSpec extends WordSpec with MustMatchers {
   case class Message4(numbers: Iterable[Int])
   case class Message5(msg: Message)
   case class Message6(msgs: Seq[Message])
+  case class Message7(msg: Option[Message])
 
   "Protobuf serializer" must {
     "serialize flat messages" in {
@@ -41,6 +42,11 @@ class ProtobufSerializerSpec extends WordSpec with MustMatchers {
     "serialize repeated embedded messages" in {
       val serializer = Protobuf.serializer[Message6]
       serializer.serialize(Message6(Seq(Message(150)))) must equal (Array(0x0a, 0x03, 0x08, 0x96, 0x01).map(_.toByte))
+    }
+    "serialize optional embedded messages" in {
+      val serializer = Protobuf.serializer[Message7]
+      serializer.serialize(Message7(Some(Message(150)))) must equal (Array(0x0a, 0x03, 0x08, 0x96, 0x01).map(_.toByte))
+      serializer.serialize(Message7(None)) must equal (Array())
     }
   }
 }
