4c6227dbb23b00e28bd9045d186f4fcc9ef129f7
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-06-11 17:22:53 +0400

    Initial commit of Benchmark app

---
+++ src/test/scala/com/alexb/protobuf/Benchmark.scala
@@ -0,0 +1,35 @@
+package com.alexb.protobuf
+
+import scala.util.Random
+
+object Benchmark extends App {
+
+  case class BenchmarkMessage(number: Int, optional: Option[Long], repeated: Seq[String], embedded: Seq[Embedded])
+  case class Embedded(fieldOne: Int, fieldTwo: Long)
+
+  def generate(count: Int): Seq[BenchmarkMessage] = Stream.continually(generate).take(count).to[Seq]
+
+  def generate =
+    BenchmarkMessage(
+      number = Random.nextInt(),
+      optional = if (Random.nextBoolean()) Some(Random.nextInt()) else None,
+      repeated = Stream.continually(Random.alphanumeric.take(32).mkString).take(Random.nextInt(100)).to[Seq],
+      embedded = Stream.continually(Embedded(fieldOne = Random.nextInt(), fieldTwo = Random.nextLong())).take(Random.nextInt(100)).to[Seq])
+
+  def run[T](data: Seq[T], serializer: Serializer[Iterable[T]]) {
+    val start = System.currentTimeMillis
+    val end = System.currentTimeMillis
+    serializer.serialize(data)
+    println(s"Took ${end - start} millis")
+  }
+
+  val data = generate(100000)
+
+  val reflectionSerializer = Protobuf.serializerForList[BenchmarkMessage]
+  val macroSerializer      = Protobuf.macroSerializerForList[BenchmarkMessage]
+
+  println("Reflection")
+  run(data, reflectionSerializer)
+  println("Macro")
+  run(data, macroSerializer)
+}
