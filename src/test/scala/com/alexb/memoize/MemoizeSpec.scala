f9cbbad4a0c43653ca3be8983e86f44a60b57116
Alexander Bulaev <aleks.bulaev@gmail.com>
2013-11-06 14:23:10 +0400

    Replace scalamock with some boilerplate in MemoizeSpec

--- src/test/scala/com/alexb/memoize/MemoizeSpec.scala
+++ src/test/scala/com/alexb/memoize/MemoizeSpec.scala
@@ -1,14 +1,13 @@
 package com.alexb.memoize
 
 import org.scalatest._
-import org.scalamock.scalatest.MockFactory
 import scala.concurrent.duration._
 import scala.concurrent.{Future, Await}
 import scala.concurrent.ExecutionContext.Implicits.global
 import Memoize._
 import language.postfixOps
 
-class MemoizeSpec extends WordSpec with MustMatchers with BeforeAndAfterEach with MockFactory {
+class MemoizeSpec extends WordSpec with MustMatchers with BeforeAndAfterEach {
 
 	implicit val cacheManager = new ConcurrentHashMapCacheManager
   val specialCacheName = "other cache"
@@ -17,50 +16,43 @@ class MemoizeSpec extends WordSpec with MustMatchers with BeforeAndAfterEach wit
 
 	"Memoize function" must {
 		"cache by-name value" in {
-      val m = mockFunction[Int]
-      m.expects().returns(1).once()
+      val m = stubFunction0(1)
 			val memoized = memoizeValue("a key", m())
 			memoized() must equal(1)
 			memoized() must equal(1)
 		}
     "cache no-arg functions" in {
-      val m = mockFunction[Int]
-      m.expects().returns(1).once()
+      val m = stubFunction0(1)
       val memoized = memoize(m)
       memoized() must equal(1)
       memoized() must equal(1)
     }
 		"cache one-arg functions" in {
-      val m = mockFunction[Int, Int]
-      m.expects(*).returns(2).once()
+      val m = stubFunction1(2)
       val memoized = memoize(m)
 			memoized(1) must equal(2)
 			memoized(1) must equal(2)
 		}
 		"cache two-arg functions" in {
-      val m = mockFunction[Int, Int, Int]
-      m.expects(*, *).returns(3).once()
+      val m = stubFunction2(3)
       val memoized = memoize(m)
 			memoized(1, 2) must equal(3)
 			memoized(1, 2) must equal(3)
 		}
 		"cache three-arg functions" in {
-      val m = mockFunction[Int, Int, Int, Int]
-      m.expects(*, *, *).returns(3).once()
+      val m = stubFunction3(3)
       val memoized = memoize(m)
 			memoized(1, 1, 1) must equal(3)
 			memoized(1, 1, 1) must equal(3)
 		}
 		"cache four-arg functions" in {
-      val m = mockFunction[Int, Int, Int, Int, Int]
-      m.expects(*, *, *, *).returns(4).once()
+      val m = stubFunction4(4)
       val memoized = memoize(m)
 			memoized(1, 1, 1, 1) must equal(4)
 			memoized(1, 1, 1, 1) must equal(4)
 		}
 		"cache no-arg functions with cache name and key" in {
-      val m = mockFunction[Int]
-      m.expects().returns(1).once()
+      val m = stubFunction0(1)
 			def memoized = memoize(specialCacheName, "key", m)
 			memoized() must equal(1)
 			memoized() must equal(1)
@@ -69,72 +61,106 @@ class MemoizeSpec extends WordSpec with MustMatchers with BeforeAndAfterEach wit
 
   "Async memoize function" must {
     "cache by-name values" in {
-      val f = stubFunction[Int]
-      f.when().returns(1)
+      val f = stubFunction0(1)
       val memoized = memoizeAsyncValue("a key", Future(f()))
       Await.result(memoized(), timeout) must equal(1)
       Thread.sleep(sleep)
       Await.result(memoized(), timeout) must equal(1)
-      f.verify().once()
     }
     "cache no-arg functions" in {
-      val f = stubFunction[Int]
-      f.when().returns(1)
+      val f = stubFunction0(1)
       val memoized = memoizeAsync(() => Future(f()))
       Await.result(memoized(), timeout) must equal(1)
       Thread.sleep(sleep)
       Await.result(memoized(), timeout) must equal(1)
-      f.verify().once()
     }
     "cache one-arg functions" in {
-      val f = stubFunction[Int, Int]
-      f.when(*).returns(2)
+      val f = stubFunction1(2)
       val memoized = memoizeAsync((a: Int) => Future(f(a)))
       Await.result(memoized(1), timeout) must equal(2)
       Thread.sleep(sleep)
       Await.result(memoized(1), timeout) must equal(2)
-      f.verify(*).once()
     }
     "cache two-arg functions" in {
-      val f = stubFunction[Int, Int, Int]
-      f.when(*, *).returns(3)
+      val f = stubFunction2(3)
       val memoized = memoizeAsync((a: Int, b: Int) => Future(f(a, b)))
       Await.result(memoized(1, 2), timeout) must equal(3)
       Thread.sleep(sleep)
       Await.result(memoized(1, 2), timeout) must equal(3)
-      f.verify(*, *).once()
     }
     "cache three-arg functions" in {
-      val f = stubFunction[Int, Int, Int, Int]
-      f.when(*, *, *).returns(3)
+      val f = stubFunction3(3)
       val memoized = memoizeAsync((a: Int, b: Int, c: Int) => Future(f(a, b, c)))
       Await.result(memoized(1, 1, 1), timeout) must equal(3)
       Thread.sleep(sleep)
       Await.result(memoized(1, 1, 1), timeout) must equal(3)
-      f.verify(*, *, *).once()
     }
     "cache four-arg functions" in {
-      val f = stubFunction[Int, Int, Int, Int, Int]
-      f.when(*, *, *, *).returns(4)
+      val f = stubFunction4(4)
       val memoized = memoizeAsync((a: Int, b: Int, c: Int, d: Int) => Future(f(a, b, c, d)))
       Await.result(memoized(1, 1, 1, 1), timeout) must equal(4)
       Thread.sleep(sleep)
       Await.result(memoized(1, 1, 1, 1), timeout) must equal(4)
-      f.verify(*, *, *, *).once()
     }
     "cache no-arg functions with cache name and key" in {
-      val f = stubFunction[Int]
-      f.when().returns(1)
+      val f = stubFunction0(1)
       def memoized = memoizeAsync("Cache name", "key", () => Future(f()))
       Await.result(memoized(), timeout) must equal(1)
       Thread.sleep(sleep)
       Await.result(memoized(), timeout) must equal(1)
-      f.verify().once()
     }
   }
 
   override protected def beforeEach() {
     cacheManager.clear("")
     cacheManager.clear(specialCacheName)
+  }
+
+  trait CalledOnlyOnce {
+    private var called = false
+
+    def incOrThrow() {
+      this.synchronized {
+        if (!called)
+          called = true
+        else
+          throw new IllegalStateException
+      }
+    }
+  }
+
+  def stubFunction0(result: Int) = new Function0[Int] with CalledOnlyOnce {
+    def apply(): Int = {
+      incOrThrow()
+      result
+    }
+  }
+
+  def stubFunction1(result: Int) = new Function1[Int, Int] with CalledOnlyOnce {
+    def apply(v1: Int): Int = {
+      incOrThrow()
+      result
+    }
+  }
+
+  def stubFunction2(result: Int) = new Function2[Int, Int, Int] with CalledOnlyOnce {
+    def apply(v1: Int, v2: Int): Int = {
+      incOrThrow()
+      result
+    }
+  }
+
+  def stubFunction3(result: Int) = new Function3[Int, Int, Int, Int] with CalledOnlyOnce {
+    def apply(v1: Int, v2: Int, v3: Int): Int = {
+      incOrThrow()
+      result
+    }
+  }
+
+  def stubFunction4(result: Int) = new Function4[Int, Int, Int, Int, Int] with CalledOnlyOnce {
+    def apply(v1: Int, v2: Int, v3: Int, v4: Int): Int = {
+      incOrThrow()
+      result
+    }
   }
 }
