be057cc564e316c1324c9ebe3d6680652d105f12
Alexander Bulaev <aleks.bulaev@gmail.com>
2014-01-19 21:34:10 +0400

    Minor: move around

--- src/main/scala/com/alexb/cassandra/Session.scala
+++ src/main/scala/com/alexb/cassandra/Session.scala
@@ -9,12 +9,6 @@ class Session(underlying: DatastaxSession) {
   def execute(query: String, values: Any*): ResultSet = underlying.execute(query, values)
   def execute(statement: Statement): ResultSet = underlying.execute(statement)
 
-  private def translateFuture[T](future: ListenableFuture[T]): Future[T] = {
-    val listener = new FutureListener[T]
-    Futures.addCallback(future, listener)
-    listener.future
-  }
-
   def executeAsync(query: String): Future[ResultSet] = translateFuture(underlying.executeAsync(query))
   def executeAsync(query: String, values: Any*): Future[ResultSet] = translateFuture(underlying.executeAsync(query, values))
   def executeAsync(statement: Statement): Future[ResultSet] = translateFuture(underlying.executeAsync(statement))
@@ -25,4 +19,10 @@ class Session(underlying: DatastaxSession) {
   def shutdown(): Future[Void] = translateFuture(underlying.shutdown())
 
   def session: DatastaxSession = underlying
+
+  private def translateFuture[T](future: ListenableFuture[T]): Future[T] = {
+    val listener = new FutureListener[T]
+    Futures.addCallback(future, listener)
+    listener.future
+  }
 }
