495b151e1d88340ea69615481f814de97e5225c8
Alexander Bulaev <aleks.bulaev@gmail.com>
2014-01-19 18:20:09 +0400

    Resolve last deprecation

--- src/test/scala/com/alexb/zk/ZkPropertiesSpec.scala
+++ src/test/scala/com/alexb/zk/ZkPropertiesSpec.scala
@@ -1,13 +1,12 @@
 package com.alexb.zk
 
-import org.scalatest.{Ignore, BeforeAndAfterAll, WordSpec}
-import org.scalatest.matchers.MustMatchers
+import org.scalatest.{Ignore, BeforeAndAfterAll, Matchers, WordSpec}
 import org.apache.zookeeper.{CreateMode, ZooKeeper}
 import com.alexb.test.Config
 import org.apache.zookeeper.ZooDefs.Ids
 
 @Ignore
-class ZkPropertiesSpec extends WordSpec with BeforeAndAfterAll with MustMatchers with Config {
+class ZkPropertiesSpec extends WordSpec with BeforeAndAfterAll with Matchers with Config {
   val connectString = config.getString("zk.connectString")
   val root = "/test"
   val zkSessionTimeoutMillis = 12000
@@ -29,39 +28,39 @@ class ZkPropertiesSpec extends WordSpec with BeforeAndAfterAll with MustMatchers
     zkProperties.close()
   } */
 
-  "ZkProperties" must {
+  "ZkProperties" should {
     "get properties" in {
       zk.create(s"$root/prop1", "data".getBytes, acl, CreateMode.PERSISTENT)
       Thread.sleep(sleepMillis)
-      zkProperties("prop1") must be (Some("data"))
+      zkProperties("prop1") should be (Some("data"))
     }
     "update properties" in {
       zk.setData(s"$root/prop1", "new_data".getBytes, -1)
       Thread.sleep(sleepMillis)
-      zkProperties("prop1") must be (Some("new_data"))
+      zkProperties("prop1") should be (Some("new_data"))
     }
     "get newly created properties" in {
       zk.create(s"$root/prop2", "some_data".getBytes, acl, CreateMode.PERSISTENT)
       Thread.sleep(sleepMillis)
-      zkProperties("prop2") must be (Some("some_data"))
+      zkProperties("prop2") should be (Some("some_data"))
     }
     "create, update and remove properties" in {
       zkProperties.put("prop3", true)
       Thread.sleep(sleepMillis)
-      zkProperties.getBoolean("prop3") must be (Some(true))
+      zkProperties.getBoolean("prop3") should be (Some(true))
 
       zkProperties.put("prop3", false)
       Thread.sleep(sleepMillis)
-      zkProperties.getBoolean("prop3") must be (Some(false))
+      zkProperties.getBoolean("prop3") should be (Some(false))
 
       zkProperties.remove("prop3")
       Thread.sleep(sleepMillis)
-      zkProperties.getBoolean("prop3") must be (None)
+      zkProperties.getBoolean("prop3") should be (None)
     }
     "delete removed properties" in {
       zk.delete(s"$root/prop1", -1)
       Thread.sleep(sleepMillis)
-      zkProperties("prop1") must be (None)
+      zkProperties("prop1") should be (None)
     }
   }
 }
