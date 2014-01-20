54b64cd5039f92c2a6a8e241886d179418aa2bd2
Alexander Bulaev <aleks.bulaev@gmail.com>
2014-01-20 13:12:08 +0400

    Move out future translation

--- src/main/scala/com/alexb/cassandra/FutureListener.scala
+++ src/main/scala/com/alexb/cassandra/FutureListener.scala
@@ -1,11 +1,19 @@
 package com.alexb.cassandra
 
 import scala.concurrent.{Future, Promise}
-import com.google.common.util.concurrent.FutureCallback
+import com.google.common.util.concurrent.{Futures => GuavaFutures, ListenableFuture, FutureCallback}
 
 private class FutureListener[T] extends FutureCallback[T] {
   private val promise: Promise[T] = Promise()
   val future: Future[T] = promise.future
   def onSuccess(result: T) = promise.success(result)
   def onFailure(t: Throwable) = promise.failure(t)
+}
+
+object Futures {
+  def fromListenableFuture[T](future: ListenableFuture[T]): Future[T] = {
+    val listener = new FutureListener[T]
+    GuavaFutures.addCallback(future, listener)
+    listener.future
+  }
 }
--- src/main/scala/com/alexb/cassandra/Session.scala
+++ src/main/scala/com/alexb/cassandra/Session.scala
@@ -1,28 +1,22 @@
 package com.alexb.cassandra
 
 import com.datastax.driver.core.{Session => DatastaxSession, _}
-import com.google.common.util.concurrent.{ListenableFuture, Futures}
 import scala.concurrent.Future
+import Futures._
 
 class Session(underlying: DatastaxSession) {
   def execute(query: String): ResultSet = underlying.execute(query)
   def execute(query: String, values: Any*): ResultSet = underlying.execute(query, values.map(_.asInstanceOf[AnyRef]): _*)
   def execute(statement: Statement): ResultSet = underlying.execute(statement)
 
-  def executeAsync(query: String): Future[ResultSet] = translateFuture(underlying.executeAsync(query))
-  def executeAsync(query: String, values: Any*): Future[ResultSet] = translateFuture(underlying.executeAsync(query, values.map(_.asInstanceOf[AnyRef]): _*))
-  def executeAsync(statement: Statement): Future[ResultSet] = translateFuture(underlying.executeAsync(statement))
+  def executeAsync(query: String): Future[ResultSet] = fromListenableFuture(underlying.executeAsync(query))
+  def executeAsync(query: String, values: Any*): Future[ResultSet] = fromListenableFuture(underlying.executeAsync(query, values.map(_.asInstanceOf[AnyRef]): _*))
+  def executeAsync(statement: Statement): Future[ResultSet] = fromListenableFuture(underlying.executeAsync(statement))
 
   def prepare(query: String): PreparedStatement = new PreparedStatement(underlying.prepare(query))
   def prepare(statement: RegularStatement): PreparedStatement = new PreparedStatement(underlying.prepare(statement))
 
-  def shutdown(): Future[Void] = translateFuture(underlying.shutdown())
+  def shutdown(): Future[Void] = fromListenableFuture(underlying.shutdown())
 
   def session: DatastaxSession = underlying
-
-  private def translateFuture[T](future: ListenableFuture[T]): Future[T] = {
-    val listener = new FutureListener[T]
-    Futures.addCallback(future, listener)
-    listener.future
-  }
 }
